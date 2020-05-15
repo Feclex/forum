d7e47cbd3d21421e2c8bcedbad1ec3c7f43046f7
Corentin Tilmant <ctilmant@ipexgroup.com>
2020-05-15 20:08:09 +0200

    fix log db

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
 <?php
 // phpBB 3.3.x auto-generated configuration file
-// Do not change anything in this file!
-$dbms = 'phpbb\\db\\driver\\mysqli';
-$dbhost = '';
-$dbport = '';
-$dbname = 'forum';
-$dbuser = 'root';
-$dbpasswd = '';
+// Do not change anything in this file! 
+$dbms = 'mysql';
+$dbhost = 'dwzeawwfeclex.mysql.db';
+$dbport = '21';
+$dbname = 'dwzeaww';
+$dbuser = 'dwzeawwfeclex';
+$dbpasswd = 'Coco4973';
 $table_prefix = 'phpbb_';
 $phpbb_adm_relative_path = 'adm/';
-$acm_type = 'phpbb\\cache\\driver\\file';
-
+$acm_type = 'file';
 @define('PHPBB_INSTALLED', true);
 @define('PHPBB_ENVIRONMENT', 'production');
 // @define('DEBUG_CONTAINER', true);
